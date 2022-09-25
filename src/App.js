d735619f4d44d4faae3893293451cc17effa9421
Bishal Rai <bishal6193@gmail.com>
2022-09-25 20:37:10 +0545

    API address and a key is provided to fetch a data.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 import './App.css';
 
+//API address and the key has been provided.
+const URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=';
+const API_KEY ='911b3806a1cbe40dacf96b52c007b3f3'
+
+
+
 function App() {
   //define state variable
   const [eur, setEur] = useState(0);
@@ -10,7 +16,8 @@ function App() {
 
   //defination of function & fetching a data using async-await method through JSON
   async function convert(e){
-    
+    //this section stop to reload the whole page, rather then refresh specific portion.
+    e.preventDefault();
     try{
       //setting address and the API key to fetch the data
       const address = URL + API_KEY;
@@ -20,7 +27,11 @@ function App() {
         //fetch the data from given API and put that into const json
         const json = await response.json();
 
-        console.log(json.rates.GBP);
+        // this section is for to have a look of the structure of the data that has been fetched from https connection so that specific info can be used.
+        // console.log(json);
+
+        //console.log(json.rates.GBP);
+
         setRate(json.rates.GBP);
 
         //fetching a conversion rate of GBP from AIP and seting a value of GBP by making a calculation
@@ -47,19 +58,19 @@ function App() {
       <form onSubmit={convert}>
         <div>
           <label>Exchange Rate: </label>
-          <label>1 € = <output>{rate}</output> £</label>
+          <label>1 € = <output>{rate.toFixed(2)}</output> £</label>
 
         </div>
 
         <div>
           <label>Euro </label>
-          <input type="number" step=""
-            value={eur} onChange={e => setEur(e.target.value)} />
+          <input type="number" step="0.01"
+        value={eur} onChange={e => setEur(e.target.value)} />
         </div>
 
         <div>
           <label>GBP : </label>
-          <output>{gbp } £</output>
+          <output>{gbp.toFixed(2)} £</output>
         </div>
 
         <div>
